628a3ae8c3c960ff9a058afeeddb3a8b1f1b2837
ranzhengwu <ranzhengwu@163.com>
2020-08-04 15:36:29 +0800

    idea修改：增加！

--- spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
+++ spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 	
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
-        return "hello "+name+"，this is first messge";
+        return "hello "+name+"，this is first messge！";
     }
 }
